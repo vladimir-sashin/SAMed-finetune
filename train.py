7ae41ae89003e5f75dc1bd21a74c312d65dd6a0b
vladimir-sashin <sashin.v.a@gmail.com>
2023-07-22 21:43:35 +0200

    Added training on custom YOLO dataset with augmentations, losses logging, val step

--- train.py
+++ train.py
@@ -8,18 +8,22 @@ import torch.backends.cudnn as cudnn
 
 from importlib import import_module
 
+import yaml
+from ultralytics.utils import IterableSimpleNamespace
+
 from sam_lora_image_encoder import LoRA_Sam
 from segment_anything import sam_model_registry
 
-from trainer import trainer_synapse
+from trainer import trainer_yolo, DEVICE
 from icecream import ic
 
+
+
 parser = argparse.ArgumentParser()
-parser.add_argument('--root_path', type=str,
-                    default='/data/LarryXu/Synapse/preprocessed_data/train_npz', help='root dir for data')
+parser.add_argument('--dataset_path', type=str, help='filepath to YOLO dataset yaml')
 parser.add_argument('--output', type=str, default='/output/sam/results')
 parser.add_argument('--dataset', type=str,
-                    default='Synapse', help='experiment_name')
+                    default='YOLO', help='experiment_name')
 parser.add_argument('--list_dir', type=str,
                     default='./lists/lists_Synapse', help='list dir')
 parser.add_argument('--num_classes', type=int,
@@ -32,7 +36,6 @@ parser.add_argument('--stop_epoch', type=int,
                     default=160, help='maximum epoch number to train')
 parser.add_argument('--batch_size', type=int,
                     default=12, help='batch_size per gpu')
-parser.add_argument('--n_gpu', type=int, default=2, help='total gpu')
 parser.add_argument('--deterministic', type=int, default=1,
                     help='whether use deterministic training')
 parser.add_argument('--base_lr', type=float, default=0.005,
@@ -53,6 +56,9 @@ parser.add_argument('--warmup_period', type=int, default=250,
 parser.add_argument('--AdamW', action='store_true', help='If activated, use AdamW to finetune SAM model')
 parser.add_argument('--module', type=str, default='sam_lora_image_encoder')
 parser.add_argument('--dice_param', type=float, default=0.8)
+parser.add_argument('--yolo_cfg', type=str, default='default_cfg.yaml',
+                    help='Path to YOLO config to control data augmentations, do not change any '
+                         'parameters in default config except augmentations')
 args = parser.parse_args()
 
 if __name__ == "__main__":
@@ -68,13 +74,6 @@ if __name__ == "__main__":
     torch.manual_seed(args.seed)
     torch.cuda.manual_seed(args.seed)
     dataset_name = args.dataset
-    dataset_config = {
-        'Synapse': {
-            'root_path': args.root_path,
-            'list_dir': args.list_dir,
-            'num_classes': args.num_classes,
-        }
-    }
     args.is_pretrain = True
     args.exp = dataset_name + '_' + str(args.img_size)
     snapshot_path = os.path.join(args.output, "{}".format(args.exp))
@@ -97,7 +96,7 @@ if __name__ == "__main__":
                                                                 pixel_std=[1, 1, 1])
 
     pkg = import_module(args.module)
-    net = pkg.LoRA_Sam(sam, args.rank).cuda()
+    net = pkg.LoRA_Sam(sam, args.rank).to(DEVICE)  # .cuda()
 
     # net = LoRA_Sam(sam, args.rank).cuda()
     if args.lora_ckpt is not None:
@@ -118,5 +117,9 @@ if __name__ == "__main__":
     with open(config_file, 'w') as f:
         f.writelines(config_items)
 
-    trainer = {'Synapse': trainer_synapse}
-    trainer[dataset_name](args, net, snapshot_path, multimask_output, low_res)
+    trainer = {'YOLO': trainer_yolo}
+
+    with open(args.yolo_cfg, "r") as f:
+        yolo_cfg = IterableSimpleNamespace(**yaml.safe_load(f))
+
+    trainer[dataset_name](args, yolo_cfg, net, snapshot_path, multimask_output, low_res)
